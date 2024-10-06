f1e05d7ae5723cfb818dea24341b828c313d59c5
Immortal215 <sharulshah@icloud.com>
2024-10-06 12:27:52 -0500

    Initial Commit

---
+++ Drawing Board/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drawing Board
+//
+//  Created by Sharul Shah on 10/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drawing Board/Drawing_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_BoardApp.swift
+//  Drawing Board
+//
+//  Created by Sharul Shah on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
