0512657c0435faf5f5bf49e3bbf1217d1dfaf171
Sharul Shah <sharulshah@icloud.com>
2024-10-22 13:58:06 -0500

    Update MyApp.swift

--- Drawing Board/MyApp.swift
+++ Drawing Board/MyApp.swift
@@ -1,14 +1,11 @@
 import SwiftUI
+import Pow
 
 @main
-
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
             Start()
-                .preferredColorScheme(.dark)
         }
-
     }
 }
-
